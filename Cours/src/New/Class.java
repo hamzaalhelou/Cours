6f704a2516850ad481327eded3fa38c37296c09d
Hamza AlHelou <125726722+hamzaalhelou@users.noreply.github.com>
2023-07-20 19:34:11 +0300

    Add files via upload

---
+++ Cours/src/New/Class.java
@@ -0,0 +1,22 @@
+
+package New;
+
+public class Class {
+    public static void main(String[] args) {
+        int age = 21;
+        float avarage = 50.0f;
+        double salary = 1250.55;
+        boolean isRegistered = false;
+        char gender = 'M';
+        String name = "Ahamd";
+        System.out.println("Name:" + name + "\n" +
+                "Age:" + age + "\n" + "gender:" + gender + "\n" +
+                        "isRegistered:" + isRegistered + "\n" +
+                                "salary:" + salary + "\n" + "average:" + avarage + "\n");
+        
+                                       
+                          
+        }
+    }
+    
+
---
+++ Cours/src/New/Method.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package New;
+
+/**
+ *
+ * @author M
+ */
+public class Method {
+    public static void welcomMassege() {
+            System.out.println("Welcom in Java");
+            
+        
+    }
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        welcomMassege();
+        System.out.println(sum(10,5));
+        
+    }
+}
+    
